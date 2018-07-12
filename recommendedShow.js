bf1325e6bafd7f020185eb129fb3012ba56149a9
github username <github email address>
2018-07-12 15:19:36 -0400

    quiz fixed

--- recommendedShow.js
+++ recommendedShow.js
@@ -1,18 +1,21 @@
-let dummyObj =
-{
-  "my_results": {
-    "mal_id": 532,
-    "url": "https://myanimelist.net/anime/532/Bishoujo_Senshi_Sailor_Moon_S",
-    "image_url": "https://myanimelist.cdn-dena.com/r/100x140/images/anime/1077/92346.jpg?s=d7690bece25e8fab50b9e916645cd3d2",
-    "title": "Bishoujo Senshi Sailor Moon S",
-    "description": "This season makes a turning point in the Sailor Moon story. The Sailor Senshi are confronted by a new enemy, the Death Busters. Rei had a premonition that this enemy would rule the world in an era cal...",
-    "type": "TV",
-    "score": 7.9,
-    "episodes": 38,
-    "members": 81347
-  },
-  "my_message": "hello"
-}
+// let dummyObj =
+// {
+//   "my_results": {
+//     "mal_id": 532,
+//     "url": "https://myanimelist.net/anime/532/Bishoujo_Senshi_Sailor_Moon_S",
+//     "image_url": "https://myanimelist.cdn-dena.com/r/100x140/images/anime/1077/92346.jpg?s=d7690bece25e8fab50b9e916645cd3d2",
+//     "title": "Bishoujo Senshi Sailor Moon S",
+//     "description": "This season makes a turning point in the Sailor Moon story. The Sailor Senshi are confronted by a new enemy, the Death Busters. Rei had a premonition that this enemy would rule the world in an era cal...",
+//     "type": "TV",
+//     "score": 7.9,
+//     "episodes": 38,
+//     "members": 81347
+//   },
+//   "my_message": "hello"
+// }
+const searchPage = document.getElementById("search-page");
+const animeShowPage = document.getElementById("anime-show-page");
+
 
 const renderRecommendedShow = (response) => {
   const animeImage = document.querySelector('.anime-image')
@@ -25,4 +28,6 @@ const renderRecommendedShow = (response) => {
       animeImage.setAttribute('src', imageUrl);
       animeTitle.innerText = title;
       animeDescription.innerText = description;
+    $(searchPage).hide()
+    $(animeShowPage).slideDown("medium")
 }
