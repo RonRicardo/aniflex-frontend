833794bd18251961fcd182a2f7a6f9b00f1c5933
pablopawpaw <lpault29@gmail.com>
2018-07-13 15:15:48 -0400

    render users lists

--- menu.js
+++ menu.js
@@ -8,16 +8,35 @@ const listPage = document.getElementById("list-page")
 const searchPage = document.getElementById("search-page")
 const animeShowPage = document.getElementById("anime-show-page")
 const welcomePage = document.getElementById("welcome-page")
+const listOfLists = document.getElementById("list-of-lists")
 
 function renderLists() {
   console.log('lists');
   if(userId.dataset.id !== "") {
     togglePages(menuPage)
+    listOfLists.innerHTML = ""
+    getLists()
     togglePages(listPage)
   } else {
     alert("Please login to see your lists")
     $(menuPage).hide()
     togglePages(welcomePage)
+  }
+}
+
+function getLists() {
+  return fetch('http://localhost:3000/api/v1/watch_lists')
+    .then(res => res.json())
+    .then(data => new Set(data.filter(el=>el.user_id===Number(userId.dataset.id)).map(e=>e.name)))
+    .then(lists => {
+      lists.forEach(list => renderListsInLists(list))
+    })
+}
+
+function renderListsInLists(list) {
+  if(list !== "") {
+    return listOfLists.innerHTML += `<h4 class="list-group-item-action d-flex justify-content-between align-items-center">
+    ${list}</h4>`
   }
 }
 
